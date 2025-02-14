8b7dd2a35b88c2c6c45f5adbda443eed457caffc
Juninho Gava <juninhog@gmail.com>
2025-02-14 12:05:31 -0300

    Criado pagina de login basica pegando o token da api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './paginas/login/login.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: '/home', pathMatch: 'full'},
+    {path:'home', component: HomeComponent, canActivate: [AuthGuard]},
+    {path:'login', component: LoginComponent}
+];
---
+++ src/app/paginas/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { ContainerComponent } from "../../componentes/container/container.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { LoginService } from '../../services/login.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    ContainerComponent, 
+    CommonModule,
+    ReactiveFormsModule
+    ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+
+  loginForm!: FormGroup;
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private activateRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.inicializarFormulario();
+  }
+
+  inicializarFormulario() {
+    this.loginForm = new FormGroup({
+      login: new FormControl('', Validators.required),
+      senha: new FormControl('', Validators.required)
+    })
+  }
+
+  logar(): void {
+    const login = this.loginForm.value;
+
+    this.loginService.logar(login).subscribe((token) => {
+      if (token != '') {
+        this.loginService.autenticar(token);
+        this.router.navigateByUrl('/home');
+      }else{
+        console.log('Usuário ou senha inválida!');
+        
+      }
+    }
+    )
+  }
+}
